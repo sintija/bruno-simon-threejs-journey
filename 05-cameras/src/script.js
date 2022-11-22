d367d3a1343b7e298e6a363027b73b673eff77e4
Sintija Linuza <sintija.linuza@gmail.com>
2022-11-22 22:54:14 +0000

    camera angles

---
+++ 05-cameras/src/script.js
@@ -0,0 +1,96 @@
+import './style.css'
+import * as THREE from 'three'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+
+
+
+
+// Cursor
+//add starting values in the object similar to size
+const cursor = {
+ x: 0,
+ y:0,
+}
+
+
+//get the coordinates of the mouse 
+window.addEventListener('mousemove', (event) => {
+    //adjusting cursors value to go from 0-1 (-0.5 to make the value go from -0.5 to 0.5) to ensure camera gan go further on the left and right
+    cursor.x = event.clientX / sizes.width - 0.5;
+    cursor.y = -(event.clientY / sizes.height - 0.5);
+    //console.log(cursor.y);
+
+})
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+scene.add(mesh)
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height,0.1 , 100)
+
+
+
+camera.position.z = 3
+camera.lookAt(mesh.position)
+scene.add(camera)
+
+//Controls 
+const controls = new OrbitControls(camera, canvas);
+// controls.target.y =2;
+// controls.update();
+controls.enableDamping = true;
+
+
+
+
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+// Animate
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    // mesh.rotation.y = elapsedTime;
+
+    //update the camera using orbit controls
+
+    //update controls 
+    controls.update();
+    
+
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
